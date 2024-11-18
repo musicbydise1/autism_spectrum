5768d60d682a7c3abaeae7e636a5b07e19a4362b
aksultanyessenbay <aksultane912@gmail.com>
2024-11-18 15:47:25 +0500

    Adding CORS

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,18 @@ import helmet from "helmet";
 import * as dotenv from "dotenv";
 
 dotenv.config({ path: `.${process.env.NODE_ENV}.env` });
+
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
 
+  // Настройка CORS
+  app.enableCors({
+    origin: ['http://localhost:8080', 'https://adminspectrum.kz'], // Разрешенные источники
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
 
   await app.listen(3000, () => {
     log("Service started on port " + PORT);
